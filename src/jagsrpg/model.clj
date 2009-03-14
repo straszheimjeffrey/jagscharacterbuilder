7f4a37ea6b4c5616ce5a1c90c1a4448fc6b8af2f
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2009-03-14 15:31:33 -0400

    Most derived stats

--- src/jagsrpg/model.clj
+++ src/jagsrpg/model.clj
@@ -14,7 +14,8 @@
 ;;  Created 14 March 2009
 
 (ns jagsrpg.model.clj
-  (:use jls.dataflow.dataflow))
+  (:use jls.dataflow.dataflow)
+  (:use [clojure.contrib.math :only (round)]))
 
 (defn var-from-name
   "Given a symbol x, return ?x"
@@ -46,6 +47,43 @@
   [stat]
   `(cell ~'cp-cost (primary-stat-cost-table ~(var-from-name stat))))
 
+(def build-modifier-table
+     (make-table 7 [-4 -2 -1 0 1 2 4 7 11 18 23 28 38]))
+
+(defn compute-grapple-mod
+  "Compute the grapple mod from multiple skills, mods is a collection
+   of [mod, skill-level] pairs"
+  [mods]
+  (if (empty? mods)
+    0
+    (let [sorted (-> mods sort reverse)
+          [highest _] (first sorted)
+          others (next sorted)
+          step (fn [_ level]
+                 (cond
+                  (= level 2) 1
+                  (= level 3) 2
+                  (= level 4) 4))]
+      (apply + highest (map step others)))))
+
+(def walking-speed-table
+     (make-table 7 [1 1 2 3 3 3 4 4 4 4 4 4]))
+(def running-speed-table
+     (make-table 7 [2 3 3 4 4 4 5 5 5 8 8 10]))
+(def sprinting-speed-table
+     (make-table 7 [3 4 5 6 6 6 7 7 7 10 10 12]))
+
+(defn make-speed-function
+  "Builds the funciton to compute speed"
+  [table per-2-rea]
+  (fn [n] (if (> n 18)
+            (+ (table 18) (* per-2-rea (quot (- n 18) 2)))
+            (table n))))
+
+(def compute-walking-speed (make-speed-function walking-speed-table 4))
+(def compute-running-speed (make-speed-function running-speed-table 2))
+(def compute-sprinting-speed (make-speed-function sprinting-speed-table 2))
+
 (defn build-main-character-model
   "Returns a collection of cells defining the core model of a JAGS character"
   []
@@ -73,11 +111,30 @@
    (secondary-stat wil int)
 
    (cell base-damage (apply + (- ?str 10) (quot (- ?bld 10) 5) ?*base-damage-mods))
+   (cell hand-to-hand-damage (apply + ?base-damage ?*hand-to-hand-damage-mods))
 
    (cell charm (apply + 10 ?*charm-mods))
    (cell intimidate (apply + 10 ?*intimidate-mods))
    (cell persuade (apply + 10 ?*persuade-mods))
    (cell recruit (apply + 10 ?*recruit-mods))
+
+   (cell damage-points (apply + ?con (build-modifier-table ?bld) ?*damage-points-mods))
+
+   (cell base-grapple (+ ?str (quot ?bld 5)))
+   (cell defensive-grapple (apply + ?base-grapple ?*defensive-grapple-mods))
+   (cell offensive-grapple (let [base (apply + ?base-grapple ?*offensive-grapple-mods)]
+                             (max (+ base 2) (round (* base 1.2)))))
+
+   (cell defensive-grapple-mods (compute-grapple-mod ?*defensive-grapple-skill-mods))
+   (cell offensive-grapple-mods (compute-grapple-mod ?*offensive-grapple-skill-mods))
+
+   (cell walking-ground-speed (apply + (compute-walking-speed ?rea) ?*walking-ground-speed-mods))
+   (cell running-ground-speed (apply + (compute-running-speed ?rea) ?*running-ground-speed-mods))
+   (cell sprinting-ground-speed (apply + (compute-sprinting-speed ?rea) ?*sprinting-ground-speed-mods))
+
+   (cell initiative (apply + ?rea ?*initiative-mods))
+   
+   (cell perception (apply + ?res ?*perception-mods))
 ])   
 
 (comment
