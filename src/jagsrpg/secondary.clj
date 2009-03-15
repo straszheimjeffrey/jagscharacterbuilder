87f9bda2e9443a31165f1573903dd1c5dc2fb5a2
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2009-03-15 00:57:29 -0400

    Begin secondary

---
+++ src/jagsrpg/secondary.clj
@@ -0,0 +1,23 @@
+;;  Copyright (c) Jeffrey Straszheim. All rights reserved.  The use and
+;;  distribution terms for this software are covered by the Eclipse Public
+;;  License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;  be found in the file epl-v10.html at the root of this distribution.  By
+;;  using this software in any fashion, you are agreeing to be bound by the
+;;  terms of this license.  You must not remove this notice, or any other,
+;;  from this software.
+;;
+;;  secondary.clj
+;;
+;;  Secondary Trait Modifiers
+;;
+;;  straszheimjeffrey (gmail)
+;;  Created 15 March 2009
+
+(ns jagsrpg.secondary
+  (:use jagsrpg.model)
+  (:use jls.dataflow.dataflow))
+
+
+
+;; End of file
+
