87593a93496cb923684a6e5d8df8a6f80c65f8b9
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2009-03-17 19:55:41 -0400

    reordered secondaries

--- src/jagsrpg/secondary.clj
+++ src/jagsrpg/secondary.clj
@@ -20,50 +20,19 @@
 (def secondary-traits
      [(standard-secondary-trait powerful      str phy :increase)
       (standard-secondary-trait weaker        str phy :decrease)
-      (standard-secondary-trait big-tall      base-bld phy :increase)
-      (standard-secondary-trait slight        base-bld phy :decrease)
-      (standard-secondary-trait resiliant     con phy :increase)
-      (standard-secondary-trait sickly        con phy :decrease)
-      (standard-secondary-trait coordinated   cor ref :increase)
-      (standard-secondary-trait klutz         cor ref :decrease)
-      (standard-secondary-trait quick         rea ref :increase)
-      (standard-secondary-trait slow          rea ref :decrease)
-      (standard-secondary-trait agile         agi ref :increase)
-      (standard-secondary-trait clumsy        agi ref :decrease)
-      (standard-secondary-trait smart         res int :increase)
-      (standard-secondary-trait dull-simple   res int :decrease)
-      (standard-secondary-trait photographic-memory mem int :increase)
-      (standard-secondary-trait forgetfull    res int  :decrease)
-      (standard-secondary-trait strong-willed wil int :increase)
-      (standard-secondary-trait weak-willed   wil int :decrease)
-
       (basic-secondary-trait "Puny" -5 str phy
                              [(cell str-mods (- 8 ?phy))])
-      (basic-secondary-trait "Emaciated" -5 bld phy
-                             [(cell bld-mods (- 7 ?phy))
-                              (cell damage-point-mods -4)])
-      (basic-secondary-trait "Fragile" -7 con phy
-                             [(cell con-mods (- 7 ?phy))])
-      (basic-secondary-trait "Crippled Hands" -6 cor ref
-                             [(cell cor-mods (- 7 ?ref))])
-      (basic-secondary-trait "Sluggish" -7 rea ref
-                             [(cell rea-mods (- 7 ?ref))])
-      (basic-secondary-trait "Uncoordinated" -8 agi ref
-                             [(cell agi-mods (- 7 ?ref))])
-      (basic-secondary-trait "Clueless" -4 res int nil)
-      (basic-secondary-trait "Retarded" -5 res int
-                             [(cell res-mods (- 7 ?int))])
-      (basic-secondary-trait "Empty Headed" -7 mem int
-                             [(cell mem-mods (- 7 ?int))])
-      (basic-secondary-trait "Wishy Washy" -4 wil int nil)
-      (basic-secondary-trait "Weak Minded" -5 wil int
-                             [(cell wil-mods (- 7 ?int))])
 
+      (standard-secondary-trait big-tall      base-bld phy :increase)
+      (standard-secondary-trait slight        base-bld phy :decrease)
       (variable-trait "Light/Short"
                       :secondary
                       (make-modifiable light-short [1 3] 1)
                       (cell cp-cost 0)
                       [(cell displayed-bld-mods (* -1 ?light-short))])
+      (basic-secondary-trait "Emaciated" -5 bld phy
+                             [(cell bld-mods (- 7 ?phy))
+                              (cell damage-point-mods -4)])
       (variable-trait "Fat/Obese"
                       :secondary
                       (make-modifiable fat-obese [1 2] 1)
@@ -72,11 +41,48 @@
                        (cell damage-points-mods ([1 3] (dec ?fat-obese)))
                        (cell agi-mods ([0 -2] (dec ?fat-obese)))
                        (cell charm-mods -2)])
+
+      (standard-secondary-trait resiliant     con phy :increase)
+      (standard-secondary-trait sickly        con phy :decrease)
+      (basic-secondary-trait "Fragile" -7 con phy
+                             [(cell con-mods (- 7 ?phy))])
       (variable-trait "Ill"
                       :secondary
                       (make-modifiable ill [1 3] 1)
                       (cell cp-cost ([-1 -3 -5] (dec ?ill)))
-                      nil)])
+                      nil)
+
+      (standard-secondary-trait coordinated   cor ref :increase)
+      (standard-secondary-trait klutz         cor ref :decrease)
+      (basic-secondary-trait "Crippled Hands" -6 cor ref
+                             [(cell cor-mods (- 7 ?ref))])
+
+      (standard-secondary-trait quick         rea ref :increase)
+      (standard-secondary-trait slow          rea ref :decrease)
+      (basic-secondary-trait "Sluggish" -7 rea ref
+                             [(cell rea-mods (- 7 ?ref))])
+
+      (standard-secondary-trait agile         agi ref :increase)
+      (standard-secondary-trait clumsy        agi ref :decrease)
+      (basic-secondary-trait "Uncoordinated" -8 agi ref
+                             [(cell agi-mods (- 7 ?ref))])
+
+      (standard-secondary-trait smart         res int :increase)
+      (standard-secondary-trait dull-simple   res int :decrease)
+      (basic-secondary-trait "Clueless" -4 res int nil)
+      (basic-secondary-trait "Retarded" -5 res int
+                             [(cell res-mods (- 7 ?int))])
+
+      (standard-secondary-trait photographic-memory mem int :increase)
+      (standard-secondary-trait forgetfull    res int  :decrease)
+      (basic-secondary-trait "Empty Headed" -7 mem int
+                             [(cell mem-mods (- 7 ?int))])
+
+      (standard-secondary-trait strong-willed wil int :increase)
+      (standard-secondary-trait weak-willed   wil int :decrease)
+      (basic-secondary-trait "Wishy Washy" -4 wil int nil)
+      (basic-secondary-trait "Weak Minded" -5 wil int
+                             [(cell wil-mods (- 7 ?int))])])
 
 (comment
 
