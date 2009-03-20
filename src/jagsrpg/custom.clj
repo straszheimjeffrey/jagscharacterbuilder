88c56db101f99804db48e442860916244d21e96a
Jeffrey Straszheim <straszheimjeffrey@gmail.com>
2009-03-20 18:52:45 -0400

    added custom file

---
+++ src/jagsrpg/custom.clj
@@ -0,0 +1,26 @@
+;;  Copyright (c) Jeffrey Straszheim. All rights reserved.  The use and
+;;  distribution terms for this software are covered by the Eclipse Public
+;;  License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;  be found in the file epl-v10.html at the root of this distribution.  By
+;;  using this software in any fashion, you are agreeing to be bound by the
+;;  terms of this license.  You must not remove this notice, or any other,
+;;  from this software.
+;;
+;;  custom.clj
+;;
+;;  Custom Traits
+;;
+;;  straszheimjeffrey (gmail)
+;;  Created 20 March 2009
+
+(ns jagsrpg.custom
+  (:use jagsrpg.model)
+  (:use clojure.contrib.dataflow))
+;  (:use [clojure.contrib.math :only (round)])
+;  (:use [clojure.contrib.seq-utils :only (find-first)])
+;  (:use [clojure.contrib.except :only (throwf)]))
+
+
+
+;; End of file
+
